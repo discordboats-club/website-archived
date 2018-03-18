9d8561fcb207d702abc2f69b511f485a569fb2e9
ronthecookie <ronthecookie0101@gmail.com>
2018-03-18 19:54:27 +0200

    [frontend-js] remove jquery and use native apis instead

--- static/global.js
+++ static/global.js
@@ -1,6 +1,5 @@
 console.log('Welcome to discordboats.club! Why are you looking here? :P');
 
-$(document).ready(function() {
-    $('select').material_select();
-  });
-           
+document.querySelectorAll("select").forEach(ele => {
+  M.FormSelect.init(ele);
+});
