a5196ddb9cbf24cdd5e62b9b4bbca15c8fd4d2a6
ronthecookie <ronthecookie0101@gmail.com>
2018-03-18 21:01:07 +0200

    magic webpack stuff

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const debug = process.env.NODE_ENV !== "production";
+const webpack = require("webpack");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
+
+module.exports = {
+  context: __dirname,
+  devtool: debug ? "inline-sourcemap" : undefined,
+  mode: debug ? "development" : "production",
+  entry: "./assets/js/main.js",
+  output: {
+    path: __dirname + "/static",
+    filename: "bundle.min.js"
+  },
+  plugins: [
+    new UglifyJsPlugin({sourceMap: true}),
+    new webpack.BannerPlugin({banner: "Copyright 2018 discordboats.club. All rights reserved."})
+  ]
+};
