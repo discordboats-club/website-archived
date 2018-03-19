ae7401608c60d4eb887cd359bfb67380a8d10735
ronthecookie <ronthecookie0101@gmail.com>
2018-03-19 14:07:55 +0200

    make dashboard page give "myBots" data to EJS

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const passport = require("passport");
 const app = module.exports = express.Router();
+const r = require("../ConstantStore").r;
 
-app.get("/", (req, res) => {
-    res.render("dashboard/index", {user: req.user});
+app.get("/", async (req, res) => {
+    const myBots = await r.table("bots").filter({ownerID: req.user.id}).run();
+    res.render("dashboard/index", {user: req.user, myBots});
 });
 
 app.get("/new", (req, res) => {
