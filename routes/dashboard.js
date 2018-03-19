6545dc53a5e4173d81db8c23b497f5151629885a
ronthecookie <ronthecookie0101@gmail.com>
2018-03-19 14:23:15 +0200

    personalize main dashboard page (also bot card)

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const passport = require("passport");
+const chunk = require("chunk");
 const app = module.exports = express.Router();
 const r = require("../ConstantStore").r;
 
 app.get("/", async (req, res) => {
     const myBots = await r.table("bots").filter({ownerID: req.user.id}).run();
-    res.render("dashboard/index", {user: req.user, myBots});
+    const botChunks = chunk(myBots, 4);
+    res.render("dashboard/index", {user: req.user, myBots: botChunks, rawBots: myBots});
 });
 
 app.get("/new", (req, res) => {
