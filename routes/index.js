08eb3467f2662c8b24ff23a1ae9937b6e08e8ef1
ronthecookie <ronthecookie0101@gmail.com>
2018-03-19 14:36:45 +0200

    personalize bot page (not fully)

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const passport = require("passport");
 const chunk = require("chunk");
 const { r } = require("../ConstantStore");
+const escapeHTML = require("escape-html");
+const marked = require("marked");
 const app = module.exports = express.Router();
 
 app.get("/", async (req, res) => {
@@ -10,6 +12,9 @@ app.get("/", async (req, res) => {
     res.render("index", {user: req.user, rawBots: bots, botChunks});
 });
 
-app.get("/botpage", (req, res) => {
-    res.render("botPage", {user: req.user});
+app.get("/bot/:id", async (req, res) => {
+    const bot = await r.table("bots").get(req.params.id).run();
+    if (!bot) return res.sendStatus(404);
+    bot._markedDescription = marked(escapeHTML(bot.longDescription));
+    res.render("botPage", {user: req.user, bot});
 });
