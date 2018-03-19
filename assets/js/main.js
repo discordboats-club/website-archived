aab2b8f2b96a8ff2f2692d52767962762e607a9b
Jellz <xdjellz@gmail.com>
2018-03-19 17:46:48 +1100

    replace thing in main.js with jquery version

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,6 @@ const API = require("./APIClient");
 console.log("%c🚫 Warning! 🚫", "color: red; font-weight: bold; font-size: x-large");
 console.log("%cTyping anything here could make bad stuff happen!", "color: #e91e63; font-size: large");
 
-document.querySelectorAll("select").forEach(ele => {
-  M.FormSelect.init(ele);
+$(document).ready(function() {
+    $('select').material_select();
 });
