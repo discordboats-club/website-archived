3538652f1b8d7387ed4502bd020e371c819e69e1
Jellz <xdjellz@gmail.com>
2018-03-19 17:47:19 +1100

    re-add msg to main js file, why was that removed too?

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
-const API = require("./APIClient"); 
+const API = require("./APIClient");
+
+console.log('Welcome to discordboats.club! Why are you looking here? :P');
 
 console.log("%c🚫 Warning! 🚫", "color: red; font-weight: bold; font-size: x-large");
 console.log("%cTyping anything here could make bad stuff happen!", "color: #e91e63; font-size: large");
