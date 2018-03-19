f04765c43efec56f4907aa13add7925b06b7231a
RONTheCookie <ronthecookie0101@gmail.com>
2018-03-19 11:50:34 +0200

    font awesome update & logged in indicator

--- assets/js/global.js
+++ assets/js/global.js
@@ -6,8 +6,13 @@ console.log('Welcome to discordboats.club! Why are you looking here? :P');
 console.log("%c🚫 Warning! 🚫", "color: red; font-weight: bold; font-size: x-large");
 console.log("%cTyping anything here could make bad stuff happen!", "color: #e91e63; font-size: large");
 
-api.getMe().then(console.log).catch(console.error);
 
-$(document).ready(function() {
+$(document).ready(async () => {
     $('select').material_select();
+    try {
+        const me = await api.getMe();
+        $("#user-proflog").html(`<i class="fas fa-user-circle"></i> ${me.username}`);
+    } catch(err) {
+        console.error(err);
+    }
 });
