385142e29ce95f4feaaf71c093ed824efd95c3a7
ronthecookie <ronthecookie0101@gmail.com>
2018-03-19 14:30:17 +0200

    show actual data on index.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const passport = require("passport");
+const chunk = require("chunk");
+const { r } = require("../ConstantStore");
 const app = module.exports = express.Router();
 
-app.get("/", (req, res) => {
-    res.render("index", {user: req.user});
+app.get("/", async (req, res) => {
+    const bots = await r.table("bots").sample(4 * 2).run();
+    const botChunks = chunk(bots, 4); // 4 bots per 
+    res.render("index", {user: req.user, rawBots: bots, botChunks});
 });
 
 app.get("/botpage", (req, res) => {
