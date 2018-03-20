d55723f6832ac97d929a118f0b409628fccc71b8
RONTheCookie <ronthecookie0101@gmail.com>
2018-03-20 11:46:24 +0200

    init form select

--- assets/js/global.js
+++ assets/js/global.js
@@ -7,10 +7,10 @@ console.log('Welcome to discordboats.club! Why are you looking here? :P');
 console.log("%c🚫 Warning! 🚫", "color: red; font-weight: bold; font-size: x-large");
 console.log("%cTyping anything here could make bad stuff happen!", "color: #e91e63; font-size: large");
 
-console.log(M);
-M.AutoInit();
+
 $(document).ready(async () => {
+    M.FormSelect.init(document.querySelector("select"));
     $("#new-bot-form").submit(e => {
-        // not implemented yet
+        m.toast({html: "Coming soon!"});
     });
 });
