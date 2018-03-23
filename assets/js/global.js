0240546b634cbb85146ddfdc8391e63da2485c5d
ronthecookie <ronthecookie0101@gmail.com>
2018-03-23 11:47:46 +0300

    all sorts of stuff

--- assets/js/global.js
+++ assets/js/global.js
@@ -2,7 +2,6 @@ const APIClient = require("./APIClient");
 const $ = require("jquery");
 const M = require("materialize-css");
 const api = new APIClient();
-window.api = api;
 console.log('Welcome to discordboats.club! Why are you looking here? :P');
 
 console.log("%c🚫 Warning! 🚫", "color: red; font-weight: bold; font-size: x-large");
@@ -10,11 +9,30 @@ console.log("%cTyping anything here could make bad stuff happen!", "color: #e91e
 
 
 $(window).ready(async () => {
-    M.FormSelect.init(document.querySelector("select#newbot"), {classes: "newbot-dd-wrap"});
     if (document.location.href.includes("/dashboard/new")) {
-        document.querySelector("button#nb-submit").addEventListener("click", e => {
-            M.toast({html: "Submitted bot!"});
-            api.createBot()
+        M.FormSelect.init($("select#newbot"), {classes: "newbot-dd-wrap"});
+        $("form").submit(async e => {
+            e.preventDefault();
+            let lib = M.FormSelect.getInstance($("select")).getSelectedValues()[0];
+            if (lib === "none") lib = undefined;
+            console.log(e);
+            try { 
+                await api.createBot({
+                    name: e.target[0].value,
+                    id: e.target[1].value,
+                    library: lib,
+                    prefix: e.target[4].value,
+                    website: e.target[5].value,
+                    invite: e.target[6].value || `https://discordapp.com/oauth2/authorize?client_id=${encodeURI(e.target[1].value)}&scope=bot&permissions=0`,
+                    shortDescription: e.target[7].value,
+                    longDescription: e.target[8].value
+                });
+                M.toast({html: "Submitted bot!"});
+            } catch (error) {
+                if (error.message === "ValidationError") return M.toast({html: "Invalid details!"});
+                M.toast({html: "Error while submitting bot!"});
+                throw error;
+            }
         });
     }
 });
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,13 +3,14 @@ const passport = require("passport");
 const chunk = require("chunk");
 const app = module.exports = express.Router();
 const r = require("../ConstantStore").r;
+const Util = require("../Util");
 
 app.get("/", async (req, res) => {
-    const myBots = await r.table("bots").filter({ownerID: req.user.id}).run();
+    const myBots = (await r.table("bots").filter({ownerID: req.user.id}).run()).map(Util.attachPropBot);
     const botChunks = chunk(myBots, 4);
     res.render("dashboard/index", {user: req.user, myBots: botChunks, rawBots: myBots});
 });
 
 app.get("/new", (req, res) => {
-    res.render("dashboard/newBot", {user: req.user});
+    res.render("dashboard/newBot", {user: req.user, libs: require("./api").libList});
 });
