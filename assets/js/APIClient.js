d3e5dc049721516c90f846a3c0de88a045f46cb0
ronthecookie <ronthecookie0101@gmail.com>
2018-03-24 12:23:29 +0300

    make the api let you delete boats.

--- assets/js/APIClient.js
+++ assets/js/APIClient.js
@@ -15,7 +15,6 @@ module.exports = class APIClient {
     /**
      * @param {Object} bot 
      * @returns {Promise<Object>} this object has a `ok` property for data from the api (usually the same for all requests)
-     * @throws promise rejects with object that has a `error` property, sometimes error objects may have a `details` property.
      */
     async createBot(bot) {
         // id, shortDescription, longDescription, invite, website, library
@@ -26,6 +25,22 @@ module.exports = class APIClient {
         },
         credentials: "same-origin"
         });
+        const data = await res.json();
+        if (data.error) {
+            throw new Error(data.error);
+        } else if (data.ok) {
+            return {ok: data.ok};
+        } else {
+            throw new Error("Bad response");
+        }
+    }
+    /**
+     * Deletes a boat. (Good thing comments get removed in production builds lmao)
+     * @param {String} id
+     * @returns {Promise<Object>} json response from the api.
+     */
+    async deleteBot(id) {
+        const res = await fetch("/api/bot/"+encodeURI(id), { method: "DELETE", credentials: "same-origin" });
         const data = await res.json();
         if (data.error) {
             throw new Error(data.error);
