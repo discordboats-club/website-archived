06942c617e618babc010b8683a94c8efe0d33dd5
ronthecookie <ronthecookie0101@gmail.com>
2018-03-24 18:52:07 +0300

    smarter css, some more utils, only show delete button to owner

--- Util.js
+++ Util.js
@@ -5,12 +5,13 @@ module.exports = class Utils {
      * @param {Object} bot 
      * @returns {Object}
      */
-    static attachPropBot(bot) {
+    static attachPropBot(bot, user = {}) {
         const client = require("./ConstantStore").bot;
         const botUser = client.users.get(bot.id);
         bot.online = typeof botUser !== "undefined" ? botUser.presence.status !== "offline" : undefined; 
         bot.servers = "N/A";
         bot._markedDescription = marked(escapeHTML(bot.longDescription), {});
+        bot._ownerViewing = user.id === bot.ownerID;
         return bot;
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,15 @@ const { r } = require("../ConstantStore");
 const app = module.exports = express.Router();
 
 app.get("/", async (req, res) => {
-    const bots = (await r.table("bots").filter({verified: true}).sample(4 * 2).run()).map(Util.attachPropBot);
+    const bots = (await r.table("bots").filter({verified: true}).sample(4 * 2).run()).map(bot => Util.attachPropBot(bot, req.user));
     const botChunks = chunk(bots, 4); // 4 bots per 
     res.render("index", {user: req.user, rawBots: bots, botChunks});
 });
 
 app.get("/bot/:id", async (req, res) => {
-    const bot = Util.attachPropBot(await r.table("bots").get(req.params.id).run());
-    if (!bot) return res.sendStatus(404);
+    const rB = await r.table("bots").get(req.params.id).run();
+    if (!rB) return res.sendStatus(404);
+    const bot = Util.attachPropBot(rB, req.user);
     if (!bot.verified && !(req.user && req.user.id === bot.ownerID)) return res.sendStatus(404); // pretend it doesnt exist lol
     res.render("botPage", {user: req.user, bot});
 });
