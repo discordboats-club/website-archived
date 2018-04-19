987a5a6ca2bcca61edfa002222c4abb9d1de6265
ronthecookie <ronthecookie0101@gmail.com>
2018-04-19 11:40:06 +0300

    basic api and auth

--- routes/botapi.js
+++ routes/botapi.js
@@ -4,5 +4,20 @@ const Joi = require("joi");
 const { r } = require("../ConstantStore");
 const Util = require("../Util");
 app.use((req, res, next) => {
-    res.status(403).json({error: "not_authenticated"});
+    const token = req.headers.Authorization;
+    if (!token) return res.status(400).json({error: "You need a Authorization header with a valid Bot Token."});
+    const bot = (await r.table("bots").filter({apiToken: token}).run())[0];
+    if (!bot) {
+        res.status(403).json({error: "not_authenticated"});
+    } else {
+        req.bot = await Util.attachPropBot(bot);
+        next();
+    }
+});
+app.get("/", (req, res) => {
+    res.json({ok: "You found the API!"});
+});
+
+app.use((req, res) => {
+    res.json({error: "Invalid Endpoint"});
 });
