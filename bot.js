d583d9f56587b578ff6bea38cff475baa360f1b0
Jellz <xdjellz@gmail.com>
2018-04-20 15:14:52 +1000

    Fixes #25

--- bot.js
+++ bot.js
@@ -19,3 +19,17 @@ client.on("message", async (msg) => {
     }
 
 });
+
+client.on('guildMemberRemove', async (member) => {
+    const loadedBots = await r.table('bots').filter({ ownerID: member.user.id }).run();
+    if (loadedBots.length !== 0) {
+        const modChannel = client.channels.find('name', 'mod-chat');
+        modChannel.send(`<@&436737982737678346> **${member.user.tag}** (\`${member.user.id}\`) left the guild, but they have **${loadedBots.length}** bots on the list. They consist of: **${loadedBots.map(b => b.name).join(', ')}**.`);
+    }
+    if (member.bot) {
+        if (!loadedBots.map(b => b.id).includes(member.user.id)) return;
+        const bot = loadedBots[member.user.id];
+        const modChannel = client.channels.find('name', 'mod-chat');
+        modChannel.send(`<@&436737982737678346> **${member.user.tag}** (\`${member.user.id}\`) left the guild, but they are a bot currently on the list. Their owner is **${bot.ownerID}** (<@${bot.ownerID}>).)`);        
+    }
+});
