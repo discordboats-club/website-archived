02ebf52eeb02a4b1a9b85084091819aa92c07558
ronthecookie <ronthecookie0101@gmail.com>
2018-04-23 11:00:17 +0300

    get rid of elasticsearch its not working well

--- elasticrdbsync.js
+++
@@ -1,8 +0,0 @@
-const rdbelasticstream = require("rethinkdb-elasticsearch-stream");
-rdbelasticstream.default({
-    backfill: true,
-    elasticsearch: { host: "127.0.0.1", port: 9200 },
-    rethinkdb: { host: "localhost", port: 28015 },
-    tables: [{ db: "discordboatsclub", table: "bots" }],
-    watch: true
-}).then(() => console.log("ElasticRDBSync is ready."));
