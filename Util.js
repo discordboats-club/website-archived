3e69dbf37855d781a8f0a79b684a746b4b360aae
Jellz <xdjellz@gmail.com>
2018-04-28 13:02:21 +1000

    make owner tag prop

--- Util.js
+++ Util.js
@@ -16,6 +16,7 @@ module.exports = class Utils {
         bot._markedDescription = marked(escapeHTML(bot.longDescription), {});
         bot._ownerViewing = user.id === bot.ownerID;
         bot._comments = await r.table("comments").filter({botID: bot.id}).run();
+        bot._ownerTag = client.users.get(bot.ownerID).tag;
         return bot;
     }
     /**
