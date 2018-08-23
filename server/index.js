3c0984e2aae62fb4ae8591ac10a7952c2e0266b9
Jellz <xdjellz@gmail.com>
2018-08-23 22:24:57 +1000

    incomplete /api/bots

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const config = require('./config.json');
 
 const r = module.exports.r = require('rethinkdbdash')({ db: 'discordboatsclub_v2' });
 const jwtKey = module.exports.jwtKey = require('fs').readFileSync('jwt.key').toString();
 
 const port = process.env.PORT || 3000;
+
+const client = require('./client.js');
+client.login(config.token);
 
 app.use(express.json());
 app.use(require('morgan')('dev'));
@@ -19,5 +23,6 @@ app.use(require('express-jwt')({ secret: jwtKey, credentialsRequired: false }), 
 });
 
 app.use('/api/auth', require('./routes/auth.js'));
+app.use('/api/bots', require('./routes/bots.js'));
 
-app.listen(port, () => console.log('discordboats.club api listening on port ' + port));
+app.listen(port, () => console.log('[web] listening on port ' + port));
---
+++ server/util.js
@@ -0,0 +1,24 @@
+const Joi = require('joi');
+
+exports.handleJoi = (req, res, schema) => {
+    const result = Joi.validate(req.body, schema);
+    if (result.error) {
+        if (!result.error.isJoi) {
+            console.error(`Error while running Joi at ${Date.now()}: ${result.error}`);
+            res.sendStatus(500);
+            return false;
+        }
+        res.status(400).json({ error: result.error.name, details: result.error.details.map(item => item.message) });
+        return false;
+    } else return true;
+}
+
+exports.libraries = ['discordcr', 'Discord.Net', 'DSharpPlus', 'dscord', 'DiscordGo', 'Discord4j', 'JDA', 'discord.js', 'Eris', 'Discordia', 'RestCord', 'Yasmin', 'discord.py', 'disco', 'discordrb', 'discord-rs', 'Sword'];
+
+exports.filterUnexpectedData = (subject, stuffToAdd, schema) => {
+    const data = Object.assign({}, stuffToAdd);
+    Object.keys(schema.describe().children).forEach(key => {
+        data[key] = subject[key];
+    });
+    return data;
+}
