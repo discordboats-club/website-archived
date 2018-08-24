19065dc2db283c1cd747bb65aa159676a098c375
draem <owen@gimbel.ca>
2018-08-24 08:34:52 -0400

    woo

--- client/src/components/common/Header/Header.jsx
+++ client/src/components/common/Header/Header.jsx
@@ -1,21 +1,122 @@
 import React, {Component} from 'react';
 
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import PersonIcon from '@material-ui/icons/Person';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+import {observer} from 'mobx-react';
+import {observable, action} from 'mobx';
 
 import './Header.css';
 
+const loggedIn = true; // temp
+
+/*
+    -------------------------
+        AMAZING CHAT ROOM
+            v5.13
+    -------------------------
+
+draem: hello
+haxified: hi
+draem: i'm going to work on the bot cards
+haxified: ok cool but make trello things
+draem: ok
+haxified: cool
+*/
+
+@observer
 export default class Header extends Component {
+    @observable
+    menuAnchorEl = null;
+
+    @action
+    handleClick = (event) => {
+        this.menuAnchorEl = event.currentTarget;
+    }
+
+    @action
+    handleClose = () => {
+        this.menuAnchorEl = null;
+    }
+
+    getSearchInput() {
+
+        return (
+            <div className ="search">
+                <input 
+                    onKeyDown={this.handleKeyDown} 
+                    type="text"
+                    maxLength={19}
+                    placeholder="Search Bots"
+                    spellCheck="false"
+                />
+            </div>
+        );
+
+    }
+
+    handleKeyDown(event) {
+        if (event.key === "Enter") {
+
+            // search !
+
+            console.log('search boats');
+
+        }
+    }
+    
+
+    getLeftButtons() {
+        if(!loggedIn) {
+            return (
+                <Button className="loginbtn btn">
+                    Login
+                </Button>
+            );
+        } else {
+            return (
+                <div>
+                    <IconButton className="btn" aria-label="My Account" onClick={this.handleClick} aria-haspopup="true" aria-owns={this.menuAnchorEl ? 'account-menu' : null}>
+                        <PersonIcon />
+                    </IconButton>
+
+                    <Menu
+                        id="account-menu"
+                        anchorEl={this.menuAnchorEl}
+                        open={Boolean(this.menuAnchorEl)}
+                        onClose={this.handleClose}
+                    >
+                        <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                        <MenuItem onClick={this.handleClose}>Dashboard</MenuItem>
+                        <MenuItem onClick={this.handleClose}>Log Out</MenuItem>
+                    </Menu>
+                </div>
+            );
+        }
+    }
+
     render() {
         return (
             <div className="header">
                 <img src={require('../../../assets/white_boat.png')} alt=""/>
 
+                {this.getSearchInput()}
+
                 <div className="leftbuttons">
-                    <Button color="white" className="discordbtn">
-                        Our Discord
+                    {this.getLeftButtons()}
+
+                    <Button className="discordbtn btn" onClick={this.navigateToDiscord}>
+                        Discord
                     </Button>
                 </div>
             </div>
         );
+    } 
+
+    navigateToDiscord() {
+        window.location = 'https://discord.gg/BJQKpts';
     }
 }
