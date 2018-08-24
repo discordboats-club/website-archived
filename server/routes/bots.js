fc31826ecfd2f9dbefed86d90ef31681325c715e
Jellz <xdjellz@gmail.com>
2018-08-24 17:51:14 +1000

    POST /api/bots sends a message to discord now

--- server/routes/bots.js
+++ server/routes/bots.js
@@ -5,6 +5,7 @@ const randomString = require('randomstring');
 const { handleJoi, libraries, filterUnexpectedData } = require('../util.js');
 const { r } = require('../');
 const client = require('../client.js');
+const config = require('../config.json');
 
 router.post('/', async (req, res) => {
     if (!req.user) return res.sendStatus(401);
@@ -16,7 +17,7 @@ router.post('/', async (req, res) => {
     if (req.body.library && !libraries.includes(req.body.library)) return res.status(400).json({ error: 'ValidationError', details: ['Invalid library'] });
 
     const botUser = client.users.get(req.body.id) || await client.users.fetch(req.body.id);
-    const ownerUser = client.users.get(req.user.id) || await client.users.fetch(req.user.id);
+    const ownerUser = await client.users.fetch(req.user.id);
     if (!ownerUser) return res.status(400).json({ error: 'ValidationError', details: ['Owner is not in discordboats discord guild'] });
     if (!botUser) return res.status(404).json({ error: 'ValidationError', details: ['Invalid bot'] });
     if (!botUser.bot) return res.status(400).json({ error: 'ValidationError', details: ['Bot must be a bot'] });
@@ -25,7 +26,7 @@ router.post('/', async (req, res) => {
 
     const bot = filterUnexpectedData(req.body, 
         { 
-            username: botUser.username, 
+            username: botUser.username,
             avatarUrl: botUser.avatarURL(), 
             views: 0, 
             inviteClicks: 0, 
@@ -38,5 +39,10 @@ router.post('/', async (req, res) => {
     );
 
     await r.table('bots').insert(bot);
+
+    const botLogChannel = client.guilds.get(config.mainGuild).channels.find(c => c.name == 'bot-log');
+    const modRole = client.guilds.get(config.mainGuild).roles.find(r => r.name == 'Moderator');
+    await botLogChannel.send(`📥 <@${req.user.id}> added ${botUser.tag} (<@&${modRole.id}>)`);
+
     res.sendStatus(200);
 });
