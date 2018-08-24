37d440872d9f3c7f42774f3cb9fa51c5eab8870a
Robert Westbury <robert-spam@miegel.org>
2018-08-24 14:38:11 +0100

    finished store

--- client/src/store.js
+++ client/src/store.js
@@ -3,4 +3,19 @@ import {observable, action, computed} from 'mobx';
 export default class Store {
     @observable
     loggedIn = false;
+
+    @observable
+    token = null;
+
+    @action
+    logIn(token) {
+        this.token = token;
+        this.loggedIn = true
+    }
+
+    @action
+    logOut() {
+        this.token = '';
+        this.loggedIn = false;
+    }
 }
