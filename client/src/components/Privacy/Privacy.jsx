f375e480fff17724d2eba74f6e050ac1a5abc131
Jellz <xdjellz@gmail.com>
2018-08-25 22:14:26 +1000

    Privacy Policy

--- client/src/components/Privacy/Privacy.jsx
+++ client/src/components/Privacy/Privacy.jsx
@@ -6,7 +6,98 @@ export default class Privacy extends Component {
     render() {
         return (
             <div className = "Privacy">
-              <h1>Privacy</h1>
+            	<h1>discordboats.club Privacy Policy</h1>
+            	<p>Last revised: <strong>August 25, 2018</strong></p>
+            	<p>
+            	 Our privacy policy applies to information we collect when you use or access our website, application, or just interact with us. 
+            	 We may change this privacy policy from time to time. Whenever we make changes to this privacy policy, 
+            	 the changes are effective 7 days after we post the revised privacy policy 
+            	 (as indicated by revising the date at the top of our privacy policy). 
+            	 We encourage you to review our privacy policy whenever you access our services to 
+            	 stay informed about our information practices and the ways you can help protect your privacy.
+            	</p>
+
+            	<h2>Collection of information</h2>
+            	<h3>Information You Provide To Us</h3>
+            	<p>
+            	 We collect information you provide directly to us. For example, we collect information when you participate
+            	 in any interactive features of our services, fill out a form, request customer support, provide any contact or identifying 
+            	 information or otherwise communicate with us. The types of information we may collect include your username, email address, and other contact
+            	 or identifying information you choose to provide. 
+            	</p>
+
+            	<h3>Information We Collect Automatically When You Use Our Services</h3>
+            	<p>
+            	 When you access or use our services, we automatically collect information about you, including:
+            	 <ul>
+            	  <li>
+            	   <strong>Log Information</strong>: We log information about your use of our services, 
+            	   including the type of browser you use, access times, pages viewed, your IP address and the page you visited before navigating
+            	   to our services.
+            	  </li>
+            	  <li>
+            	   <strong>Information Collected by Cookies and Other Tracking Technologies</strong>: We use various technologies to collect 
+            	   information, and this may include sending cookies to your computer. Cookies are small data files stored on your hard drive or in your
+            	   device memory that helps us to improve our services and your experience, see which areas and features of our services are popular and 
+            	   count visits. For more information about cookies, visit <a href="https://cookiesandyou.com/">cookiesandyou.com</a>. 
+            	  </li>
+            	 </ul>
+            	</p>
+
+            	<h3>Information We Collect From Other Sources</h3>
+            	<p>
+            	 In order to provide you with access to our Services, or to provide you with better service in general,
+            	 we may combine information obtained from other sources (for example, a third-party service whose application you have authorized 
+            	 or used to sign in) and combine that with information we collect through our services.
+            	</p>
+
+            	<h2>Use of Information</h2>
+            	<p>
+            	 We use information about you for various purposes, including to:
+            	 <ul>
+            	  <li>Provide, maintain and improve our services;</li>
+            	  <li>Provide services you request, process transactions and to send you related information;</li>
+            	  <li>Send you technical notices, updates, security alerts and support and administrative messages</li>
+            	  <li>Respond to your comments, questions and requests and provide customer service;</li>
+            	  <li>Communicate with you about news and information related to our service;</li>
+            	  <li>Monitor and analyze trends, usage and activities in connection with our services; and</li>
+            	  <li>Personalize and improve our services.</li>
+            	 </ul>
+            	 By accessing and using our services, you consent to the processing and transfer of your information in and to the
+            	 United States and other countries.
+            	</p>
+
+            	<h2>Third Party Analytics</h2>
+            	<p>
+            	 We may allow third parties to provide analytics services. These third parties may use cookies, web beacons and other 
+            	 technologies to provide information about your use of the services and other websites, including your IP address, web browser, 
+            	 pages viewed, time spent on pages, links clicked and conversion information. This information may be used by us and third parties to, among other 
+            	 things, analyze and track data, determine the popularity of certain content and other websites and better understand your online activity.
+            	 Our privacy policy does not apply to, and we are not responsible for, third party cookies, web beacons or other tracking technologies and 
+            	 we encourage you to check the privacy policies of these third parties to learn more about their privacy practices.
+            	</p>
+
+            	<h2>Security</h2>
+            	<p>
+            	 We take reasonable measures to help protect personal information from loss, theft, misuse and unauthorized access, 
+            	 disclosure, alteration and destruction.
+            	</p>
+
+            	<h3>Cookies</h3>
+            	<p>
+            	 Most web browsers are set to accept cookies by default. If you prefer, you can usually choose to set your browser to 
+            	 remove or reject browser cookies. Please note that if you choose to remove or reject cookies, this could affect the availability 
+            	 and functionality of our services.
+            	</p>
+
+            	<h3>Promotional Communications</h3>
+            	<p>
+            	  You may opt out of receiving any promotional emails from us by following the instructions in those emails. If you opt out, 
+            	  we may still send you non-promotional communications, such as those about your account or our ongoing business relations.
+            	</p>
+
+            	<h2>Contact Us</h2>
+            	<p>If you have any questions or concerns about this privacy policy, please contact us at <a href="mailto:support@discordboats.club">support@discordboats.club</a>.</p>
             </div>
         );
     }
