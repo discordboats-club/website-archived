6dc2519e76aa377cf17c960a92138ed43e053d1c
Jellz <xdjellz@gmail.com>
2018-08-26 21:31:32 +1000

    /api documentation route on frontend

---
+++ client/src/components/APIDocs/APIDocs.jsx
@@ -0,0 +1,13 @@
+import React, { Component} from 'react';
+
+import './APIDocs.css';
+
+export default class APIDocs extends Component {
+    render() {
+        return (
+            <div className = "APIDocs">
+                  <h1>ehm hello welcome to the api documentation</h1>
+            </div>
+        );
+    }
+}
--- client/src/components/common/Router.jsx
+++ client/src/components/common/Router.jsx
@@ -5,6 +5,7 @@ import Header from './Header/Header';
 
 import Terms from '../ToS/ToS';
 import Privacy from '../Privacy/Privacy';
+import APIDocs from '../APIDocs/APIDocs';
 
 class Router extends Component {
     render() {
@@ -17,6 +18,7 @@ class Router extends Component {
                       <Switch>
                         <Route exact path="/terms" component={Terms}/>
                         <Route exact path="/privacy" component={Privacy}/>
+                        <Route exact path="/api" component={APIDocs}/>
                       </Switch>
                     </div>
                 </div>
