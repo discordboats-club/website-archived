1ea9dbf7462ce2a231d8b2e87a4845660dd30fff
Landon Gravat <304961@sps.org>
2018-08-31 15:12:03 +0000

    added front and backend for tags, also descriptions.

--- client/src/components/common/BotCard/BotCardSmall.jsx
+++ client/src/components/common/BotCard/BotCardSmall.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-import './BotCardSmall.css';
+import './BotCardSmall.css'
 
 export default class BotCardSmall extends Component {
     constructor(props) {
@@ -11,13 +11,18 @@ export default class BotCardSmall extends Component {
     
     render() {
         return (
-            <div className = "card">
-                <img src={this.bot.avatarUrl} alt = {`${this.bot.username}'s icon`} width="196px" height="196px"/>
-                <div className = "details">
-                    <h2>{this.bot.username}<small>{this.bot.premium ? '[premium]' : ''}</small></h2>
+            <div className="card">
+                <img src={this.bot.avatarUrl} alt={`${this.bot.username}'s icon`} className="image"/>
+                <div className="details">
+                    <h2>{this.bot.username + this.bot.premium ? <small>[premium]</small> : ''}</h2>
+                    <p>{this.bot.shortDesc}</p>
+                    <h5>Tags</h5>
+                    <ul className="horizontal-list">
+                        {this.bot.botTags.map(tag => {<li>{tag}</li>})}
+                    </ul>
                 </div>
             </div>
-        );
+        )
     }
 }
 
