7e14fd95ebef0090d7d7b058b871186d4025817b
Anthony <11891199+PhoenixOnARoof@users.noreply.github.com>
2018-09-09 17:19:49 -0600

    Update index.js
    The backlash was immense 
    fu devs :\

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const config = require('./config.json');
 const r = module.exports.r = require('rethinkdbdash')({ db: 'discordboatsclub_v2', port: 28016 });
 const jwtKey = module.exports.jwtKey = require('fs').readFileSync('jwt.key').toString();
 
-const port = process.env.PORT || 9998;
+const port = process.env.PORT || 3000;
 
 const client = require('./client.js');
 client.login(config.token);
