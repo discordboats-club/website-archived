0887c0cc8353b0de2904d90873fc294bf5ad9497
Anthony G <agriffith1107@gmail.com>
2018-09-30 22:44:54 +0000

    Update client.js

--- server/client.js
+++ server/client.js
@@ -14,9 +14,9 @@ client.once('ready', () => {
 client.on('message', msg => {
     if (msg.author.bot) return;
 
-    if (msg.content.indexOf('-') !== 0) return;
+    if (msg.content.indexOf('dbs') !== 0) return;
 
-    const args = msg.content.slice("-".length).trim().split(/ +/g);
+    const args = msg.content.slice("dbs".length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
     switch(command) {
