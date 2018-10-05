89079ad94e72a56cfc6f280420ccb4397582bf5b
Anthony G <agriffith1107@gmail.com>
2018-10-05 21:39:38 +0000

    added a success msg to the halloween command

--- server/client.js
+++ server/client.js
@@ -138,8 +138,10 @@ client.on('message', msg => {
             msg.channel.send(':ping_pong: Pong!');
             break;
         case 'halloween':
-            msg.member.roles.add("497874685103439874").catch(err => {
-                    msg.channel.send('Error running this command.')});
+            msg.member.roles.add("497874685103439874")
+            msg.channel.send("<:customCheck:485196148064256019> Halloween role successfully added! Enjoy being spoopy \;").catch(err => {
+                    msg.channel.send('Error running this command.')
+                    });
         break;
         case 'help':
         case 'commands':
