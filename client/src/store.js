b10fd698525783a5b50e8296196f07705b54527b
sdf <sdfsdfsdf1234@mail.com>
2018-10-08 17:47:23 +0000

    fixed the client to where it can build again

--- client/src/store.js
+++ client/src/store.js
@@ -4,10 +4,12 @@ class Store {
     authtoken = 'none';   
 }
 
+/*
 const hydrate = create({
   jsonify: true
 });
 
 hydrate('store', Store).then(() => {console.log('Store hydrated');});
+*/
 
 export default new Store();
