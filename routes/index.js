828b27e44cec2ef8f57019e6e773690a737d8ab8
sdf <sdfsdfsdf1234@mail.com>
2018-12-16 00:17:27 +0000

    trying a different rethink method

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ app.get('/browse', async (req, res) => {
 
     const start = page * itemsPerPage;
 
-    const bots = await r.table('bots').filter({ verified: true }).orderBy(r.desc('name')).between(start, start + itemsPerPage);
+    const bots = await r.table('bots').filter({ verified: true }).orderBy(r.desc('name')).slice(start, start + itemsPerPage);
     const butChunks = chunk(bots, 4);
 
     res.render('browse', { user: req.user, rawBots: bots, botChunks });
