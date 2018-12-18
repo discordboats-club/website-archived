b409588928b0cf14e623d465c76c6867c5af2604
sdf <sdfsdfsdf1234@mail.com>
2018-12-18 17:07:32 -0600

    v2 actually hosts the client files now

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,20 @@
 import Koa from 'koa';
+import mount from 'koa-mount';
+import send from 'koa-send';
+import staticDir from 'koa-static';
 import { router } from './routers/index';
 import { config } from '../config';
 
 const server = new Koa();
 const port = process.env.PORT || config.port || 3000;
 
-server.use(router.routes());
-server.use(router.allowedMethods());
+server.use(mount('/api', router.routes()));
+server.use(mount('/api', router.allowedMethods()));
+
+server.use(staticDir(`${__dirname}/../../../client/dist`));
+
+server.use(async ctx => {
+    await send(ctx, 'client/dist/index.html', { root: `${__dirname}/../../../` });
+});
 
 server.listen(port, () => console.log(`Listing on port ${port}`));
