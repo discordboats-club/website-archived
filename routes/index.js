621669b96d4791fecc4712ea971c189f95deccaa
InBlue <inblueofficial@outlook.com>
2019-01-15 07:11:54 +0000

    Fixed the expression

--- routes/index.js
+++ routes/index.js
@@ -141,7 +141,7 @@ app.get('/user/:id', async (req, res, next) => {
 app.get('/search?:q', async (req, res) => {
     if (typeof req.query.q !== 'string') return res.status(403).json({ error: 'expected query q' });
     const query = req.query.q.toLowerCase();
-    const text = req.query.q.replace(/<([^>+)/gi, "");
+    const text = req.query.q.replace(/<([^>]+)>/gi, "");
     const bots = await Promise.all(
         (await r
             .table('bots')
