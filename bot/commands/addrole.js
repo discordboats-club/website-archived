d400ab2f03b1776a2dbeceb07931d7f07cc3934d
Anthony G <agriffith1107@gmail.com>
2019-02-03 11:47:48 +0000

    Fix the audit log section?

--- bot/commands/addrole.js
+++ bot/commands/addrole.js
@@ -14,7 +14,7 @@ module.exports.run = async (client, msg, args) => {
     if (!gRole) return msg.channel.send(`<:customX:485196473441583134> Couldn't find that role`);
 
     if (member.roles.has(gRole.id)) return msg.channel.send(`<:customX:485196473441583134> That user already has that role`);
-    member.roles.add(gRole.id, `addrole cmd used to add ${gRole.name} to ${member.tag}`).then(() => {
+    member.roles.add(gRole.id, `addrole cmd used to add ${gRole.name} to ${member.user.tag}`).then(() => {
         msg.channel.send(`<:customCheck:485196148064256019> Added role **${gRole.name}** to <@${member.id}>`)
     }).catch((e) => {
         msg.channel.send(`<:customX:485196473441583134> An unexpected error has occurred while adding the role to <@${member.id}>. The error has been logged `);
