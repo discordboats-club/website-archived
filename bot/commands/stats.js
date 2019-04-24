ab532a90455c18dce0b26fd1b16e036f3896cbf9
ncsinfinty12 <49201143+ncsinfinty12@users.noreply.github.com>
2019-04-23 22:29:55 -0500

    Update stats.js

--- bot/commands/stats.js
+++ bot/commands/stats.js
@@ -22,7 +22,6 @@ exports.run = (client, message, args) => {
 		.setColor('RANDOM')
 		.setAuthor('Bot Stats')
      		.setThumbnail(message.guild.iconURL, true)
-		.setDescription("Bot Owner <@398536643172237314>" , true)
                 .addField('**<:tools:554713650707824642> VPS**', `**Hostname** - ${os.hostname()}\n **VPS Uptime** - ${os.uptime()} hours\n **CPU** - ${os.cpus().map(i => `${i.model}`)[0]}\n **CPU Cores** - ${os.cpus().length}\n **CPU Usage** - ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} %\n  **Platform** - ${os.type}\n **Memory Total** - ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB\n **Memory Used** - ${used}\n  `, true)
                 .addField("**<:tools:554713650707824642> Bot Misc Stats**", `**Commands** - ${client.commands.size}\n **Node Version** - ${process.version}`)
 		.setFooter(`Requested by: ${message.author.username}`, message.author.avatarURL)
